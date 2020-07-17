4eeb23761bcc5f02b5295f930529eb8aa02eeba9
teegre <teegre@protonmail.com>
2020-07-17 10:54:38 +0200

    created some directories

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ CONFIG_DIR="$HOME/.config/bigtime"
 ! [[ -d "$CONFIG_DIR" ]] && { mkdir -p "$CONFIG_DIR/digits"; }
 
 cp ./src/bigtime "$INSTALL_DIR/"
-cp ./src/*.digits "$CONFIG_DIR/"
-cp ./src/*.wav  "$CONFIG_DIR/"
+cp ./digits/*    "$CONFIG_DIR/"
+cp ./snd/*.wav   "$CONFIG_DIR/"

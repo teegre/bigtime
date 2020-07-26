d7202645959e6e54526ae4af15968419a29a934d
teegre <teegre@protonmail.com>
2020-07-26 22:29:00 +0200

    added manpage to install script

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 INSTALL_DIR="$HOME/.local/bin"
 CONFIG_DIR="$HOME/.config/bigtime"
+MAN_DIR="$HOME/.local/share/man/man1"
 
 ! [[ -d "$CONFIG_DIR" ]] && { mkdir -p "$CONFIG_DIR/digits"; }
 
 cp ./src/bigtime "$INSTALL_DIR/"
 cp ./digits/*    "$CONFIG_DIR/"
 cp ./snd/*.wav   "$CONFIG_DIR/"
+cp ./bigtime.1   "$MAN_DIR/"
